51e9c7ecfa935f14284a091170cc3518dc56624c
DevFrog92 <yongsu1481@gmail.com>
2024-02-05 17:00:21 +0900

    feat: implement ValidationChecker

---
+++ src/main/java/com/example/diary/domain/member/util/ValidationChecker.java
@@ -0,0 +1,24 @@
+package com.example.diary.domain.member.util;
+
+import com.example.diary.global.exception.CustomValidationException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidationChecker {
+    private ValidationChecker() {
+    }
+
+    public static void hasError(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+            }
+
+            throw new CustomValidationException(errors);
+        }
+    }
+}
