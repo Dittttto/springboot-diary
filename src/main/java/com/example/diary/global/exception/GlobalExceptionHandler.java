d4cb39c22aa7834bc6fb966679edee584a449515
DevFrog92 <yongsu1481@gmail.com>
2024-02-05 17:00:08 +0900

    refactor: update ResponseDTO

--- src/main/java/com/example/diary/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/diary/global/exception/GlobalExceptionHandler.java
@@ -1,20 +1,27 @@
 package com.example.diary.global.exception;
 
 import com.example.diary.global.web.dto.response.ResponseDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
+    // update 가 필요하다
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ResponseDTO<String>> customExceptionHandler(CustomException e) {
-        return ResponseEntity.ok(ResponseDTO.error(e.getErrorCode().getStatus(), e.getMessage()));
+    public ResponseEntity<?> customExceptionHandler(CustomException e) {
+        return new ResponseEntity<>(ResponseDTO.error(e.getErrorCode().getStatus(), e.getMessage()), e.getErrorCode().getStatus());
     }
 
     @ExceptionHandler(CustomJwtException.class)
-    public ResponseEntity<ResponseDTO<String>> customJwtExceptionHandler(CustomJwtException e) {
-        return ResponseEntity.ok(ResponseDTO.error(e.getErrorCode().getStatus(), e.getMessage()));
+    public ResponseEntity<?> customJwtExceptionHandler(CustomJwtException e) {
+        return new ResponseEntity<>(ResponseDTO.error(e.getErrorCode().getStatus(), e.getMessage()), e.getErrorCode().getStatus());
+
+    }
+
+    @ExceptionHandler(CustomValidationException.class)
+    public ResponseEntity<?> customValidationExceptionHandler(CustomValidationException e) {
+        return new ResponseEntity<>(ResponseDTO.error(HttpStatus.BAD_REQUEST, e.getErrors()), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/example/diary/global/web/dto/response/ResponseDTO.java
+++ src/main/java/com/example/diary/global/web/dto/response/ResponseDTO.java
@@ -7,11 +7,15 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public class ResponseDTO<T> {
-    private HttpStatus resultCode;
+    private HttpStatus code;
     private T body;
 
     public static ResponseDTO<String> error(HttpStatus status, String message) {
         return new ResponseDTO<>(status, message);
+    }
+
+    public static <T> ResponseDTO<T> error(HttpStatus status, T body) {
+        return new ResponseDTO<>(status, body);
     }
 
     public static <T> ResponseDTO<T> success(T body) {
