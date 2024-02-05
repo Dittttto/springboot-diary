fc0c4b3a1009315bc1b2f97dffd6c46a32340d23
DevFrog92 <yongsu1481@gmail.com>
2024-02-05 15:51:42 +0900

    feat: implement CommentService and CommentServiceImpl
    feat: implement CommentService and CommentServiceImpl

---
+++ src/main/java/com/example/diary/domain/comment/service/dto/CommentCreateDto.java
@@ -0,0 +1,22 @@
+package com.example.diary.domain.comment.service.dto;
+
+import com.example.diary.domain.member.model.Member;
+import com.example.diary.domain.schedule.model.Schedule;
+import lombok.Getter;
+
+@Getter
+public class CommentCreateDto {
+    private final Member member;
+    private final Schedule schedule;
+    private final String content;
+
+    public CommentCreateDto(
+            Member member,
+            Schedule schedule,
+            String content
+    ) {
+        this.member = member;
+        this.schedule = schedule;
+        this.content = content;
+    }
+}
