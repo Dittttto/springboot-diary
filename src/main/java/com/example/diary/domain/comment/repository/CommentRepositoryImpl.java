425c1e9a8dbc8d15271ba1fba2959c6c3be9833b
DevFrog92 <yongsu1481@gmail.com>
2024-02-05 15:51:12 +0900

    feat: implement CommentRepositoryImpl

---
+++ src/main/java/com/example/diary/domain/comment/repository/CommentRepositoryImpl.java
@@ -0,0 +1,64 @@
+package com.example.diary.domain.comment.repository;
+
+import com.example.diary.domain.comment.infrastructure.CommentJpaRepository;
+import com.example.diary.domain.comment.infrastructure.entity.CommentEntity;
+import com.example.diary.domain.comment.model.Comment;
+import com.example.diary.domain.comment.service.dto.CommentCreateDto;
+import com.example.diary.global.exception.CustomException;
+import com.example.diary.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentRepositoryImpl implements CommentRepository{
+    private final CommentJpaRepository commentJpaRepository;
+
+    @Override
+    public void register(CommentCreateDto dto) {
+        CommentEntity commentEntity = new CommentEntity(
+                dto.getSchedule().toEntity(),
+                dto.getMember().toEntity(),
+                dto.getContent()
+        );
+
+        commentJpaRepository.save(commentEntity);
+    }
+
+    @Override
+    public Comment findById(Long id) {
+        return commentJpaRepository
+                .findById(id)
+                .map(Comment::from)
+                .orElseThrow(() ->
+                        new CustomException(ErrorCode.NOT_FOUND_EXCEPTION));
+
+    }
+
+    @Override
+    public List<Comment> findByMemberId(Long id) {
+        return commentJpaRepository
+                .findAllByMemberId(id).stream()
+                .map(Comment::from)
+                .toList();
+    }
+
+    @Override
+    public Comment updateById(Long id, String content) {
+        CommentEntity entity = commentJpaRepository.findById(id)
+                .orElseThrow(() ->
+                        new CustomException(ErrorCode.NOT_FOUND_EXCEPTION));
+
+        entity.setContent(content);
+
+        commentJpaRepository.saveAndFlush(entity);
+        return Comment.from(entity);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        commentJpaRepository.deleteById(id);
+    }
+}
