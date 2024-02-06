e9b9acd85965ec4e4a8d42e5070ad453a6b9d5b3
DevFrog92 <yongsu1481@gmail.com>
2024-02-06 16:30:39 +0900

    refactor: add UN_AUTHORIZATION_EXCEPTION

--- src/main/java/com/example/diary/global/exception/ErrorCode.java
+++ src/main/java/com/example/diary/global/exception/ErrorCode.java
@@ -13,7 +13,8 @@ public enum ErrorCode {
     AUTHOR_LENGTH_EXCEPTION(HttpStatus.BAD_REQUEST, "작성자 항목은 필수 입니다."),
     REQUIRED_FIELD_EXCEPTION(HttpStatus.BAD_REQUEST, "필수 항목입니다."),
     NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "잘못된 접근입니다."),
-    PASSWORD_INVALID_EXCEPTION(HttpStatus.FORBIDDEN, "권한이 없습니다."),
+    PASSWORD_INVALID_EXCEPTION(HttpStatus.FORBIDDEN, "비밀번호가 잘못되었습니다."),
+    UN_AUTHORIZATION_EXCEPTION(HttpStatus.UNAUTHORIZED, "권한이 없습니다."),
     DUPLICATE_USER_EXCEPTION(HttpStatus.BAD_REQUEST, "이미 가입된 사용자 입니다.");
 
     private final HttpStatus status;
