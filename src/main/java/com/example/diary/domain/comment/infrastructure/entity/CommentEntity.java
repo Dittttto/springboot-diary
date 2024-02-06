142c8e9a96f292fc579456b28b342443dfc55c45
DevFrog92 <yongsu1481@gmail.com>
2024-02-06 12:46:16 +0900

    refactor: add parentCommentId and childComment

--- src/main/java/com/example/diary/domain/comment/infrastructure/entity/CommentEntity.java
+++ src/main/java/com/example/diary/domain/comment/infrastructure/entity/CommentEntity.java
@@ -3,10 +3,7 @@ package com.example.diary.domain.comment.infrastructure.entity;
 import com.example.diary.domain.member.infrastructure.entity.MemberEntity;
 import com.example.diary.domain.schedule.infrastructure.entity.ScheduleEntity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 import org.springframework.data.annotation.CreatedDate;
@@ -14,6 +11,8 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Getter
 @Table(name = "schedule_comment")
@@ -28,11 +27,22 @@ public class CommentEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JoinColumn(name = "scheduleId")
     @ManyToOne(optional = false)
     private ScheduleEntity schedule;
 
+    @JoinColumn(name = "memberId")
     @ManyToOne(optional = false)
     private MemberEntity member;
+
+    @Setter
+    @Column(updatable = false)
+    private Long parentCommentId;
+
+    @ToString.Exclude
+    @OrderBy("createdAt ASC")
+    @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL)
+    private Set<CommentEntity> childComment = new LinkedHashSet<>();
 
     @Setter
     @Column(nullable = false)
@@ -50,10 +60,25 @@ public class CommentEntity {
     public CommentEntity(
             ScheduleEntity schedule,
             MemberEntity member,
+            Long parentCommentId,
             String content
     ) {
         this.schedule = schedule;
+        this.parentCommentId = parentCommentId;
         this.member = member;
         this.content = content;
+    }
+
+    public static CommentEntity of(
+            ScheduleEntity schedule,
+            MemberEntity member,
+            String content
+    ) {
+        return new CommentEntity(schedule, member, null, content);
+    }
+
+    public void addChildComment(CommentEntity child) {
+        child.setParentCommentId(this.id);
+        this.getChildComment().add(child);
     }
 }
