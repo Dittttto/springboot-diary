49188736db3091ef5acc89a0540ed0d3eb694275
DevFrog92 <yongsu1481@gmail.com>
2024-02-06 12:47:28 +0900

    refactor: add parentCommentId and child comments

--- src/main/java/com/example/diary/domain/comment/service/dto/CommentInfoDTO.java
+++ src/main/java/com/example/diary/domain/comment/service/dto/CommentInfoDTO.java
@@ -4,9 +4,13 @@ import com.example.diary.domain.comment.model.Comment;
 import com.example.diary.domain.member.service.dto.MemberInfoDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+@ToString
 @Getter
 @AllArgsConstructor
 public class CommentInfoDTO {
@@ -14,6 +18,8 @@ public class CommentInfoDTO {
     private final MemberInfoDTO memberId;
     private final Long scheduleId;
     private final String content;
+    private Long parentCommentId;
+    private Set<CommentInfoDTO> childComments;
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
 
@@ -23,6 +29,8 @@ public class CommentInfoDTO {
                 comment.getMemberInfo(),
                 comment.getScheduleId(),
                 comment.getContent(),
+                comment.getParentCommentId(),
+                comment.getChildComment().stream().map(CommentInfoDTO::from).collect(Collectors.toSet()),
                 comment.getCreatedAt(),
                 comment.getUpdatedAt()
         );
