b91b3c4299ef2dc7f22b6a6818ca8e0afba8f9fb
DevFrog92 <yongsu1481@gmail.com>
2024-02-13 10:18:16 +0900

    refactor: add ignoreResourceNotFound in DiaryApplication

--- src/main/java/com/example/diary/DiaryApplication.java
+++ src/main/java/com/example/diary/DiaryApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@PropertySource(value = {"classpath:db.properties", "classpath:jwt.properties"})
+@PropertySource(value = {"classpath:db.properties", "classpath:jwt.properties"}, ignoreResourceNotFound = true)
 public class DiaryApplication {
 
     public static void main(String[] args) {
